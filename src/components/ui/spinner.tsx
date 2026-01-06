778a7992fde77f01deed798a5fc49550ef9745ef
lapzenstore <lapzen.store@gmail.com>
2026-01-06 18:57:22 +0500

    Update spinner.tsx

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,13 +1,11 @@
-import { Loader2Icon } from "lucide-react"
-
 import { cn } from "@/lib/utils"
+import { Loader } from "./loader"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <Loader2Icon
-      role="status"
-      aria-label="Loading"
-      className={cn("size-4 animate-spin", className)}
+    <Loader
+      size="xs"
+      className={cn("inline-flex", className)}
       {...props}
     />
   )
