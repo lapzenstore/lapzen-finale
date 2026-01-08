e9574e9c0c103ee4d626ebedc54b746a1df7ae1e
lapzenstore <lapzen.store@gmail.com>
2026-01-08 20:43:01 +0500

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,17 @@
+export const SERIES_MAPPING: { [key: string]: string } = {
+  "hp-omen": "HP Omen",
+  "hp-zbook": "HP Zbook",
+  "hp-elitebook": "HP Elitebook",
+  "dell-precision": "Dell Precision",
+  "dell-latitude": "Dell Latitude",
+  "dell-xps": "Dell XPS",
+  "lenovo-legion": "Lenovo Legion",
+  "lenovo-thinkpad": "Lenovo ThinkPad",
+  "lenovo-thinkbook": "Lenovo Thinkbook",
+  "apple-macbook": "Apple Macbook",
+  "toshiba": "Toshiba",
+  "asus": "Asus"
+};
+
+export const SERIES_NAMES = Object.values(SERIES_MAPPING);
+export const SERIES_SLUGS = Object.keys(SERIES_MAPPING);
